70a2e5741160fba1fd6359cdb04983279004c1c7
Lee <pogostanlee@gmail.com>
2022-07-12 00:06:15 +0800

    22

--- assets/js/login.js
+++ assets/js/login.js
@@ -14,15 +14,15 @@ $(function () {
           /^[\S]{6,12}$/
           ,'密码必须6到12位，且不能出现空格'
         ] 
-     'repwd': (value) => {
-         // 通过形参拿到的是确认密码框中的内容
-         // 还需要拿到密码框中的内容
-         // 然后进行一次等于的判断
-         // 如果判断失败,则return一个提示消息即可
-         var pwd = $('.reg-box [name=password]').val();
-         if (pwd !== value) {
-             return '两次密码不一致！';
-         }
-     }
+        'repwd': function(value) {
+            // 通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败,则return一个提示消息即可
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+              return '两次密码不一致！'
+            }
+          }
       })
 }) 
